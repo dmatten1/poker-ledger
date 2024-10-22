59906d35c33a7abf39c5b53536a801329bcf60a3
Daniel Matten <dcm92@duke.edu>
2024-10-22 00:08:11 -0400

    pretty sure we now have a set of entry objects

--- ledgerPage.js
+++ ledgerPage.js
@@ -1,7 +1,8 @@
 const addAllTimeButton = document.getElementById('addToAllTime');
+const entrySet = localStorage.getItem('entrySet');
+
 addAllTimeButton.addEventListener('click', () => {
     //add to the big total
-  
   
   
   
