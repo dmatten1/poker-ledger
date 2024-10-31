abaedd8487f39e6d0097b362d853b84a9c5238be
Daniel Matten <dcm92@duke.edu>
2024-10-31 19:24:40 -0400

    line break

--- allTimeStats.js
+++ allTimeStats.js
@@ -26,7 +26,6 @@ const loadMasterLedger = async () => {
                                 
                             </tr>
                         </thead>
-                        <br></br>
                         <tbody>
                 `;
 
@@ -39,6 +38,7 @@ const loadMasterLedger = async () => {
                         <td>${(item.net/item.hours).toFixed(2)}</td>
                     </tr>
                 `).join('');
+                ledgerContent += `<br></br>`;
 
                 // Close table structure
                 ledgerContent += `
